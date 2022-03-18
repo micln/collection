dc2468726a64e7fea125fa03b5efe6475fba48fa
huiren <zhrlnt@gmail.com>
2022-03-18 22:12:04 +0800

    hashset (#2)

---
+++ collection/collection.go
@@ -0,0 +1,25 @@
+package collection
+
+type BaseCollection[E any] interface {
+	Add(E)
+	Contains(E) bool
+	Remove(E)
+	Clear()
+	Size() int
+	ToSlice() []E
+}
+
+type Collection[E any] interface {
+	BaseCollection[E]
+
+	AddMany(...E)
+	AddAll(Collection[E])
+
+	ContainsMany(...E) bool
+	ContainsAll(Collection[E]) bool
+
+	RemoveMany(...E)
+	RemoveAll(Collection[E])
+
+	IsEmpty(E) bool
+}
