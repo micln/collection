4503770b758c4f6fb9dcfc4f6124eae4040be784
huiren <zhrlnt@gmail.com>
2022-03-18 21:52:14 +0800

    rm code

--- collection/collection.go
+++ collection/collection.go
@@ -16,21 +16,14 @@ type Collection[E any] interface {
 
 	iter.Iterable[E]
 
-	// Add(E)
 	AddMany(...E)
 	AddAll(Collection[E])
 
-	// Contains(E) bool
 	ContainsMany(...E) bool
 	ContainsAll(Collection[E]) bool
 
-	// Remove(E)
 	RemoveMany(...E)
 	RemoveAll(Collection[E])
 
 	IsEmpty(E) bool
-	Size() int
-	Clear()
-
-	// ToSlice() []E
 }
