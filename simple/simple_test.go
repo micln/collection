0b826d20ee5305d77fb1527ee42ffb9f2e012552
huiren <zhrlnt@gmail.com>
2022-03-19 00:45:07 +0800

    simple test

---
+++ simple/simple_test.go
@@ -0,0 +1,17 @@
+package simple
+
+import (
+	"fmt"
+)
+
+func Example() {
+	fmt.Println(Max(8, 10))                 // 10
+	fmt.Println(Max("8", "10"))             // 8
+	fmt.Println(IfThen(true, false, true))  // false
+	fmt.Println(IfThen(false, false, true)) // true
+	// Output:
+	// 10
+	// 8
+	// false
+	// true
+}
